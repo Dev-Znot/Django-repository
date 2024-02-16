bfb6c2ea319afd6da44dc57e7e59637863f55b0c
Thalles Zainote <thalles.zainote11@gmail.com>
2024-02-15 22:20:00 -0300

    Delete Banco de dados directory

--- Banco de dados/001-bd/plataforma/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Cliente(models.Model): #Cria-se o banco de dados com as respectivas info
-    nome = models.CharField(max_length=20)
-    data_nacimento = models.DateField()
-    email = models.EmailField()
-    descricao = models.CharField(max_length=100)
-
-    #Aplica-se o makemigrations e migrate
--- Banco de dados/001-bd/plataforma/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('plataforma/', views.plataforma, name='plataforma'),
-    path('usuario/', views.usuario, name='usuario'),
-]
